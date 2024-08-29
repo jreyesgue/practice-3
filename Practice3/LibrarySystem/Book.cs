530a884a243c57e7b2619fb9d04a05e351b50eee
Joel Reyes <jreyesguerrero@deloitte.com>
2024-08-28 18:08:55 -0600

    Add project files.

---
+++ Practice3/LibrarySystem/Book.cs
@@ -0,0 +1,37 @@
+﻿namespace Practice3.LibrarySystem
+{
+    public class Book
+    {
+        private bool isCheckedOut;
+        public string ISBN { get; init; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+
+        public Book(string isbn, string title, string author,
+            bool isCheckedOut = false)
+        {
+            ISBN = isbn;
+            Title = title;
+            Author = author;
+            this.isCheckedOut = isCheckedOut;
+        }
+
+        public void CheckOut()
+        {
+            isCheckedOut = true;
+        }
+
+        public void Return()
+        {
+            isCheckedOut = false;
+        }
+
+        public void DisplayDetails()
+        {
+            Console.WriteLine($"\n--- Book {ISBN} ---");
+            Console.WriteLine($"Title: {Title}");
+            Console.WriteLine($"Author: {Author}");
+            Console.WriteLine($"Is {(isCheckedOut ? "" : "Not")} Checked Out");
+        }
+    }
+}
---
+++ Practice3/LibrarySystem/Library.cs
@@ -0,0 +1,44 @@
+﻿namespace Practice3.LibrarySystem
+{
+    public class Library
+    {
+        private readonly List<Book> _books;
+
+        public Library()
+        {
+            _books = new List<Book>();
+        }
+
+        public void AddBook(Book book)
+        {
+            _books.Add(book);
+        }
+
+        public void RemoveBook(string bookIsbn)
+        {
+            _books.RemoveAll(book => book.ISBN == bookIsbn);
+        }
+
+        public void CheckOutBook(string bookIsbn)
+        {
+            Book book = FindBookByISBN(bookIsbn);
+            book.CheckOut();
+        }
+
+        public void ReturnBook(string bookIsbn)
+        {
+            Book book = FindBookByISBN(bookIsbn);
+            book.Return();
+        }
+
+        public void ListAllBooks()
+        {
+            _books.ForEach(book => book.DisplayDetails());
+        }
+
+        private Book FindBookByISBN(string bookIsbn)
+        {
+            return _books.Find(book => book.ISBN == bookIsbn);
+        }
+    }
+}
